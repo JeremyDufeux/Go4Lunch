4308f695f98c8a983721e7bfb15472c15b7027d6
Jeremy <jeremy.dufeux@gmail.com>
2021-02-28 16:09:04 +0100

    Create MainActivity with LoginFragment and Navigation Component

---
+++ app/src/main/java/com/jeremydufeux/go4lunch/LoginFragment.java
@@ -0,0 +1,31 @@
+package com.jeremydufeux.go4lunch;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LoginFragment extends Fragment {
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+    public static LoginFragment newInstance() {
+        return new LoginFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
