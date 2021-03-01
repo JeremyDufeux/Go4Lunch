6aac944b1e54cd5b9f7ee7cfc4d6bac05d27a9bb
Jeremy <jeremy.dufeux@gmail.com>
2021-03-01 11:50:04 +0100

    setup navigation with bottom nav bar and toolbar in MainActivity

--- app/src/main/java/com/jeremydufeux/go4lunch/MainActivity.java
+++ app/src/main/java/com/jeremydufeux/go4lunch/MainActivity.java
@@ -1,14 +1,54 @@
 package com.jeremydufeux.go4lunch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+import com.jeremydufeux.go4lunch.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
+
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = mBinding.getRoot();
+        setContentView(view);
+
+        configureBottomNavigationView();
+        configureToolbar();
+    }
+
+    private void configureBottomNavigationView() {
+        BottomNavigationView bottomNavigationView = mBinding.bottomNavigationView;
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.main_activity_nav_host);
+        NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.getNavController());
+    }
+
+    private void configureToolbar() {
+        mToolbar = mBinding.mainActivityToolbar;
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //2 - Inflate the menu and add it to the Toolbar
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
     }
 }
