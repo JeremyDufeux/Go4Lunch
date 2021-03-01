8dc05f6fcb91f5d56604799683c847fa4a1ea0d8
Jeremy <jeremy.dufeux@gmail.com>
2021-03-01 18:42:15 +0100

    add WorkmateHelper for firestore

---
+++ app/src/main/java/com/jeremydufeux/go4lunch/api/WorkmateHelper.java
@@ -0,0 +1,53 @@
+package com.jeremydufeux.go4lunch.api;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.jeremydufeux.go4lunch.models.Workmate;
+
+import java.util.Date;
+
+public class WorkmateHelper {
+
+    private static final String COLLECTION_NAME = "workmates";
+
+    // --- Collection ---
+    public static CollectionReference getWorkmatesCollection(){
+        return FirebaseFirestore.getInstance().collection(COLLECTION_NAME);
+    }
+
+    // --- Create ---
+    public static Task<Void> createWorkmate(String uid, String username, String email, String urlPicture) {
+        Workmate workmateToCreate = new Workmate(uid, username, email, urlPicture);
+        return WorkmateHelper.getWorkmatesCollection().document(uid).set(workmateToCreate);
+    }
+
+    // --- Get ---
+    public static Task<DocumentSnapshot> getId(String uid){
+        return WorkmateHelper.getWorkmatesCollection().document(uid).get();
+    }
+
+    // --- Update ---
+    public static Task<Void> updateUserName(String userName, String uid) {
+        return WorkmateHelper.getWorkmatesCollection().document(uid).update("userName", userName);
+    }
+
+    public static Task<Void> updateEmail(String uid, String email) {
+        return WorkmateHelper.getWorkmatesCollection().document(uid).update("email", email);
+    }
+
+    public static Task<Void> updateChosenRestaurantId(String uid, String chosenRestaurantId) {
+        return WorkmateHelper.getWorkmatesCollection().document(uid).update("chosenRestaurantId", chosenRestaurantId);
+    }
+
+    public static Task<Void> updateLastChosenRestaurantDate(String uid, Date lastChosenRestaurantDate) {
+        return WorkmateHelper.getWorkmatesCollection().document(uid).update("lastChosenRestaurantDate", lastChosenRestaurantDate);
+    }
+
+    // --- Delete ---
+    public static Task<Void> deleteWorkmate(String uid) {
+        return WorkmateHelper.getWorkmatesCollection().document(uid).delete();
+    }
+
+}
