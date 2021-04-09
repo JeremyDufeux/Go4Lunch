8205f3c1469c32825c4a9fe090054dd1eac6c637
Jeremy <jeremy.dufeux@gmail.com>
2021-04-09 18:05:19 +0200

    use AsyncListDiffer

--- app/src/main/java/com/jeremydufeux/go4lunch/ui/fragment/workmates/WorkmatesAdapter.java
+++ app/src/main/java/com/jeremydufeux/go4lunch/ui/fragment/workmates/WorkmatesAdapter.java
@@ -6,13 +6,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.AsyncListDiffer;
+import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.jeremydufeux.go4lunch.R;
 import com.jeremydufeux.go4lunch.databinding.FragmentWorkmateItemBinding;
+import com.jeremydufeux.go4lunch.models.Restaurant;
 import com.jeremydufeux.go4lunch.models.Workmate;
+import com.jeremydufeux.go4lunch.ui.fragment.listView.ListViewPlacesAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,10 +24,11 @@ import java.util.List;
 public class WorkmatesAdapter extends RecyclerView.Adapter<WorkmatesAdapter.WorkmateViewHolder>{
 
     private final OnWorkmateListener mWorkmateListener;
-    private final List<Workmate> mWorkmateList = new ArrayList<>();
+    private final AsyncListDiffer<Workmate> mWorkmateList;
 
     public WorkmatesAdapter(OnWorkmateListener workmateListener) {
         mWorkmateListener = workmateListener;
+        mWorkmateList = new AsyncListDiffer<>(this, new DifferCallback());
     }
 
     @NonNull
@@ -35,19 +40,17 @@ public class WorkmatesAdapter extends RecyclerView.Adapter<WorkmatesAdapter.Work
 
     @Override
     public void onBindViewHolder(@NonNull WorkmateViewHolder holder, int position) {
-        holder.updateViewHolder(mWorkmateList.get(position));
+        holder.updateViewHolder(mWorkmateList.getCurrentList().get(position));
 
     }
 
     @Override
     public int getItemCount() {
-        return mWorkmateList.size();
+        return mWorkmateList.getCurrentList().size();
     }
 
     public void updateList(List<Workmate> workmates) {
-        mWorkmateList.clear();
-        mWorkmateList.addAll(workmates);
-        notifyDataSetChanged();
+        mWorkmateList.submitList(workmates);
     }
 
     static class WorkmateViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
@@ -94,5 +97,18 @@ public class WorkmatesAdapter extends RecyclerView.Adapter<WorkmatesAdapter.Work
 
     public interface OnWorkmateListener {
         void onWorkmateClick(int position);
+    }
+
+    public static class DifferCallback extends DiffUtil.ItemCallback<Workmate> {
+        public boolean areItemsTheSame(Workmate oldItem, Workmate newItem) {
+            return oldItem.getUId().equals(newItem.getUId());
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Workmate oldItem, @NonNull Workmate newItem) {
+            return oldItem.getNickname().equals(newItem.getNickname())
+                    && oldItem.getChosenRestaurantName().equals(newItem.getChosenRestaurantName())
+                    && oldItem.getPictureUrl().equals(newItem.getPictureUrl());
+        }
     }
 }
